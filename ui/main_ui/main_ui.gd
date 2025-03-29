f5b5356e5ca5522573cd272efa549a7047f74c64
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2025-03-29 11:28:15 +0100

    feat: Player selection system
    Players can now be selected by clicking on them
    Clicking anywhere on the background will unselect the currently selected player
    The player score edition panel will only be visible if a player is selected

--- ui/main_ui/main_ui.gd
+++ ui/main_ui/main_ui.gd
@@ -1,14 +1,48 @@
 class_name MainUI
-extends Node
+extends Control
 
 @onready var add_player_button: Button = %AddPlayerButton
 @onready var player_list: Control = %PlayerList
+@onready var player_edit_panel: Control = %PlayerEditPanel
 
 var player_ui_scene: PackedScene = preload("res://ui/player_ui/player_ui.tscn")
 
+var selected_player: PlayerUI = null
+
 func _ready() -> void:
 	add_player_button.pressed.connect(_on_add_player)
+	player_edit_panel.visible = false
 	
 	
 func _on_add_player() -> void:
-	player_list.add_child(player_ui_scene.instantiate())
+	var player_ui: PlayerUI = player_ui_scene.instantiate()
+	player_list.add_child(player_ui)
+	
+	player_ui.selected.connect(_on_player_select.bind(player_ui))
+	player_ui.selected.emit()
+	
+	player_ui.deleted.connect(_on_player_delete.bind(player_ui))
+
+	
+func _on_player_select(player_ui: PlayerUI) -> void:
+	if selected_player != null:
+		selected_player.unselect()
+	
+	player_ui.select()
+	selected_player = player_ui
+	player_edit_panel.visible = true
+	
+
+func _on_player_delete(player_ui: PlayerUI) -> void:
+	if selected_player == player_ui:
+		player_ui.unselect()
+		selected_player = null
+		player_edit_panel.visible = false
+	
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and selected_player != null:
+			selected_player.unselect()
+			selected_player = null
+			player_edit_panel.visible = false
--- ui/player_ui/player_ui.gd
+++ ui/player_ui/player_ui.gd
@@ -1,11 +1,30 @@
 class_name PlayerUI
-extends Node
+extends PanelContainer
+
+signal selected
+signal deleted
 
 @onready var delete_button: Button = %DeleteButton
 
 func _ready() -> void:
 	delete_button.pressed.connect(_on_player_delete)
-	
+
 	
 func _on_player_delete() -> void:
+	deleted.emit()
 	self.queue_free()
+	
+	
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			selected.emit()
+			
+			
+func select() -> void:
+	self.theme_type_variation = "Selected"
+	
+	
+func unselect() -> void:
+	self.theme_type_variation = ""
+			
